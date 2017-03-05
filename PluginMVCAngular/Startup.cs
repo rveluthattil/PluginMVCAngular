c1e37ec002c55d46043d01b3e204eaca33fd60e5
ram-synechron <Ramachandran.Veluthattil@ingramcontent.com>
2017-03-04 23:50:28 -0600

    initial commit

---
+++ PluginMVCAngular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluginMVCAngular.Startup))]
+namespace PluginMVCAngular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
